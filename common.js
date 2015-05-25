0e887e6d9a6c10857ba05ca0384d3716dc052a8f
Mateusz Kocz <mateusz.kocz@nopattern.net>
2015-05-25 10:04:01 +0200

    feat(js): mock local storage.

--- common.js
+++ common.js
@@ -9,17 +9,32 @@ angular.module( '4screens.common', [] );
 
 'use strict';
 
-angular.module('4screens.common').factory( 'CommonLocalStorageService',
-  ["localStorageService", function( localStorageService ) {
-    return {
-      get: function( key ) {
-        return localStorageService.get( key );
-      },
-      set: function( key, value ) {
-        localStorageService.set( key, value );
-      }
+angular.module('4screens.common').provider( 'CommonLocalStorageService', function() {
+    var mock = false;
+    var mockedStore = {};
+
+    this.mock = function() {
+      mock = true;
     };
-  }]
+
+    this.$get = ["localStorageService", function(localStorageService) {
+      return {
+        get: function(key) {
+          if (mock) {
+            return mockedStore[key];
+          }
+          return localStorageService.get(key);
+        },
+        set: function(key, value) {
+          if (mock) {
+            mockedStore[key] = value;
+          } else {
+            localStorageService.set(key, value);
+          }
+        }
+      };
+    }]
+  }
 );
 
 'use strict';
--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,14 +1,29 @@
 'use strict';
 
-angular.module('4screens.common').factory( 'CommonLocalStorageService',
-  function( localStorageService ) {
-    return {
-      get: function( key ) {
-        return localStorageService.get( key );
-      },
-      set: function( key, value ) {
-        localStorageService.set( key, value );
-      }
+angular.module('4screens.common').provider( 'CommonLocalStorageService', function() {
+    var mock = false;
+    var mockedStore = {};
+
+    this.mock = function() {
+      mock = true;
     };
+
+    this.$get = function(localStorageService) {
+      return {
+        get: function(key) {
+          if (mock) {
+            return mockedStore[key];
+          }
+          return localStorageService.get(key);
+        },
+        set: function(key, value) {
+          if (mock) {
+            mockedStore[key] = value;
+          } else {
+            localStorageService.set(key, value);
+          }
+        }
+      };
+    }
   }
 );
